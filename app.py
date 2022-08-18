05a08ddc12b16ce76a4aea438f4b1bc27537244a
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-08-18 09:57:32 +0100

    organize files

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 # Imports
 # --------------------------------------------------------------------------- #
 
+import json
 import sys
+import dateutil.parser
+import babel
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_moment import Moment
 import logging
@@ -22,6 +25,21 @@ moment = Moment(app)
 app.config.from_object('config')
 db.init_app(app)
 migrate = Migrate(app, db)
+
+
+# --------------------------------------------------------------------------- #
+# Filters.
+# --------------------------------------------------------------------------- #
+
+def format_datetime(value, format='medium'):
+  date = dateutil.parser.parse(value)
+  if format == 'full':
+      format="EEEE MMMM, d, y 'at' h:mma"
+  elif format == 'medium':
+      format="EE MM, dd, y h:mma"
+  return babel.dates.format_datetime(date, format, locale='en')
+
+app.jinja_env.filters['datetime'] = format_datetime
 
 
 # --------------------------------------------------------------------------- #
