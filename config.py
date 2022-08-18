98c41087e38b9570ac41a8776af9b2e2d8c4fe69
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-08-18 05:47:20 +0100

    implement functionalities

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+# Disable modification tracking system
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:chinecherem11@localhost:5432/fyyur'
+# Todo Completed ✓
---
+++ models.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    facebook_link = db.Column(db.String(500), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    website = db.Column(db.String(500), nullable=False)
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue', lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self): 
+      return f'<Venue id:{self.id} name:{self.name} state:{self.state} city:{self.city}>'
+
+#-------------------------------------------------------------------------------------------------#
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    facebook_link = db.Column(db.String(500), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    website = db.Column(db.String(500), nullable=False)
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True, cascade="all, delete-orphan")
+
+    def __repr__(self):
+      return f'<Artist id:{self.id} name:{self.name} state:{self.state} city:{self.city}>'
+
+#-------------------------------------------------------------------------------------------------#
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+      return f'<Show id:{self.id} venue_id:{self.venue_id} artist_id:{self.artist_id} start_time:{self.start_time}'
